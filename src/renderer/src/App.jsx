e64c7f49e1f607a38dc50afd2dd28127651b1b8e
Angel <acalmenc@gmail.com>
2026-03-25 20:16:14 -0400

    added libraries for project

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>Electron + Vite</h1>
+    <div className="bg-gray-100 blur-smoke min-h-screen">
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-3xl font-serif italic font-normal ">Electron + Vite</h1>
+      </div>
     </div>
   )
 }
